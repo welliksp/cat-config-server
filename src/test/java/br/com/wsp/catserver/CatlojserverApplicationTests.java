ecaf52d2795a9e9f4dfa5d64894eec88a6366d6b
Wellik Perroni <welliksp@gmail.com>
2023-06-28 16:42:52 -0300

    add application

---
+++ src/test/java/br/com/wsp/catserver/CatlojserverApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.wsp.catserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatlojserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
